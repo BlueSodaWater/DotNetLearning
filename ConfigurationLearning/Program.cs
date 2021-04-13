54097935cfaab0c59d4e917d6bbae95b77765786
tide Blue <835059949@qq.com>
2021-04-13 16:56:15 +0800

    How to use Configuration

---
+++ ConfigurationLearning/Program.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace ConfigurationLearning
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using IHost host = CreateHostBuilder(args).Build();
+
+            await host.RunAsync();
+        }
+
+        // Json
+        //static IHostBuilder CreateHostBuilder(string[] args) =>
+        //    Host.CreateDefaultBuilder(args)
+        //        .ConfigureAppConfiguration((hostingContext, configuration) =>
+        //        {
+        //            configuration.Sources.Clear();
+
+        //            IHostEnvironment env = hostingContext.HostingEnvironment;
+
+        //            var path = AppDomain.CurrentDomain.BaseDirectory;
+        //            string projectDirectory = Directory.GetParent(path).Parent.Parent.Parent.FullName;
+        //            configuration
+        //                .AddJsonFile($"{projectDirectory}//appsettings.json", optional: true, reloadOnChange: true)
+        //                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", true, true);
+
+        //            IConfigurationRoot configurationRoot = configuration.Build();
+
+        //            TransientFaultHandlingOptions options = new();
+        //            configurationRoot.GetSection(nameof(TransientFaultHandlingOptions))
+        //                .Bind(options);
+
+        //            Console.WriteLine($"TransientFaultHandlingOptions.Enabled={options.Enabled}");
+        //            Console.WriteLine($"TransientFaultHandlingOptions.AutoRetryDelay={options.AutoRetryDelay}");
+        //        });
+
+        // xml
+        static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((hostingContext, configuration) =>
+                {
+                    configuration.Sources.Clear();
+
+                    var path = AppDomain.CurrentDomain.BaseDirectory;
+                    string projectDirectory = Directory.GetParent(path).Parent.Parent.Parent.FullName;
+                    configuration
+                        .AddXmlFile($"{projectDirectory}//appsettings.xml", optional: true, reloadOnChange: true)
+                        .AddXmlFile($"repeating-example.xml", true, true);
+
+                    configuration.AddEnvironmentVariables();
+
+                    if (args is { Length: > 0 })
+                    {
+                        configuration.AddCommandLine(args);
+                    }
+                });
+    }
+}
