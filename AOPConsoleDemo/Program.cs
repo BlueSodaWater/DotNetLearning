5b0a6f060d69c8c28ec9f7d9aaf16ee7b3278648
tide Blue <835059949@qq.com>
2021-04-15 10:33:11 +0800

    How to use AOP

---
+++ AOPConsoleDemo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Reflection;
+
+namespace AOPConsoleDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("========原始需求=========");
+            User user = new User { Name = "Zoe", Age = 18 };
+            IUserService userService = new UserService();
+            userService.AddUser(user);
+
+            Console.WriteLine("========动态代理 实现新需求=========");
+            IUserService userService1 = DispatchProxy.Create<IUserService, MyProxy>();
+            ((MyProxy)userService1).TargetClass = new UserService();
+            userService1.AddUser(user);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ AOPConsoleDemo/UserService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AOPConsoleDemo
+{
+    public class UserService : IUserService
+    {
+        public bool AddUser(User user)
+        {
+            Console.WriteLine("用户添加成功");
+            return true;
+        }
+    }
+}
